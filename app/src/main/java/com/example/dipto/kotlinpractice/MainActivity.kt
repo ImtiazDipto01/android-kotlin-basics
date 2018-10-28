e7e278275b5e232e772d7b2fc53ef54076365fd4
imtiaz <programmer.imtiaz@gmail.com>
2018-10-29 01:36:19 +0600

    ADD- click listener added with when clause

--- app/src/main/java/com/example/dipto/kotlinpractice/MainActivity.kt
+++ app/src/main/java/com/example/dipto/kotlinpractice/MainActivity.kt
@@ -2,16 +2,18 @@ package com.example.dipto.kotlinpractice
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.TextView
 import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
 
     lateinit var name : String
     lateinit var email : String
     lateinit var msg : String
     var age : Int = 0
+    var count = 0
 
     lateinit var tvHello : TextView
     lateinit var tvname : TextView
@@ -48,5 +50,22 @@ class MainActivity : AppCompatActivity() {
         tvname = findViewById<TextView>(R.id.tv_my_name)
         tvAge = findViewById<TextView>(R.id.tv_my_age)
         tvEmail = findViewById<TextView>(R.id.tv_my_email)
+        btnCount.setOnClickListener(this)
+        btnReset.setOnClickListener(this)
+    }
+
+    override fun onClick(v: View?) {
+        if (v != null) {
+            when(v.id){
+                R.id.btnCount ->{
+                    count ++
+                    tvCountValue.setText(count.toString())
+                }
+                R.id.btnReset ->{
+                    count = 0
+                    tvCountValue.setText(count.toString())
+                }
+            }
+        }
     }
 }
