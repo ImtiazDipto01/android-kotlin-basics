0611ded48f2db4fd88fba116704b9abcc7e6455d
imtiaz <programmer.imtiaz@gmail.com>
2018-11-03 02:06:19 +0600

    ADD- SecondActivity added and startActivity added for button click listener

--- app/src/main/java/com/example/dipto/kotlinpractice/MainActivity.kt
+++ app/src/main/java/com/example/dipto/kotlinpractice/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.dipto.kotlinpractice
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
@@ -67,7 +68,8 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
                     tvCountValue.setText(count.toString())
                 }
                 R.id.btnNewActivity ->{
-
+                    val intent = Intent(this, SecondActivity::class.java)
+                    startActivity(intent)
                 }
             }
         }
